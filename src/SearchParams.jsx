1cc8db02e8726ec34b72c523fd5edf6be6eff9c0
khaled Abdulhalim <kabdulhalim@reseau.free.fr>
2024-09-16 14:53:03 +0200

    TailwindCSS

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -18,8 +18,9 @@ const SearchParams = () => {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -33,12 +34,19 @@ const SearchParams = () => {
       >
         <label htmlFor="location">
           Location
-          <input id="location" name="location" placeholder="Location" />
+          <input
+            type="text"
+            id="location"
+            name="location"
+            placeholder="Location"
+            className="search-input"
+          />
         </label>
 
         <label htmlFor="animal">
           Animal
           <select
+            className="search-input"
             id="animal"
             name="animal"
             onChange={(e) => {
@@ -59,7 +67,12 @@ const SearchParams = () => {
 
         <label htmlFor="breed">
           Breed
-          <select disabled={!breeds.length} id="breed" name="breed">
+          <select
+            className="search-input grayed-out-disabled"
+            disabled={!breeds.length}
+            id="breed"
+            name="breed"
+          >
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -69,7 +82,9 @@ const SearchParams = () => {
           </select>
         </label>
 
-        <button>Submit</button>
+        <button className="rounded px-6 py-2 text-white hover:opacity-50 border-none bg-orange-500">
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
